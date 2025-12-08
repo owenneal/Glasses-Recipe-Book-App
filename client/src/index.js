4e7ce384bcc67e555b16dcc81340b841ae15043e
owen neal <ocneal1@gmail.com>
2025-12-08 14:31:08 -0600

    update title in index.html to reflect app name and add modern.css for styling

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './modern.css'; //change this line to change the style sheet
 
 ReactDOM.render(
   <React.StrictMode>
